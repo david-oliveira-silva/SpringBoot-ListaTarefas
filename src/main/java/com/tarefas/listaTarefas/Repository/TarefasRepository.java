9485e481f32494baec4deb7fcfdea0ae6b40569d
davidOiveira <143909652+davidOiveira@users.noreply.github.com>
2024-04-27 20:14:23 -0300

    correção do repository

--- src/main/java/com/tarefas/listaTarefas/Repository/TarefasRepository.java
+++ src/main/java/com/tarefas/listaTarefas/Repository/TarefasRepository.java
@@ -2,6 +2,8 @@ package com.tarefas.listaTarefas.Repository;
 
 import com.tarefas.listaTarefas.Models.TarefasModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TarefasRepository extends JpaRepository<TarefasModel,Long> {
 }
