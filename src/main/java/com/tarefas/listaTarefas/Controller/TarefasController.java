a689b39bf9b533710ff6d2ef88f60fe1933c17e3
davidOiveira <143909652+davidOiveira@users.noreply.github.com>
2024-04-27 22:48:10 -0300

    Criado o metodo getAll e getById

--- src/main/java/com/tarefas/listaTarefas/Controller/TarefasController.java
+++ src/main/java/com/tarefas/listaTarefas/Controller/TarefasController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TarefasController {
@@ -24,5 +26,17 @@ public class TarefasController {
         BeanUtils.copyProperties(tarefasDTO,TarefasModel);
         return ResponseEntity.status(HttpStatus.OK).body(tarefasRepository.save(TarefasModel));
 
+    }
+    @GetMapping("/tarefas")
+    public ResponseEntity<List<TarefasModel>> getTarefas(){
+        return ResponseEntity.status(HttpStatus.OK).body(tarefasRepository.findAll());
+    }
+    @GetMapping("/tarefas/{id}")
+    public ResponseEntity<Object> getByIdTarefas(@PathVariable(name = "id")Long id){
+        Optional<TarefasModel> tarefa0 = tarefasRepository.findById(id) ;
+        if (tarefa0.isEmpty()){
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tarefa não encontrada");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(tarefasRepository.findById(id));
     }
 }
